e5a8dd95b6c4b85131ff36b32f5389a6f30efc9b
VuHoangNguyen <vuhoangnguyen49@gmail.com>
2018-08-15 14:29:34 +0700

    finished

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-( function( $ ) {
-  $(".nav_btn").on("click",function(){
+(function($) {
+  $(".nav_btn").on("click", function() {
     $(this).toggleClass('nav_btn_actived');
     $(this).next().toggleClass('nav_actived');
+    return false;
   });
-} )( jQuery );
+})(jQuery);
